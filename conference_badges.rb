385c10069a6aee3b0ee5f3ec134c29e321045cdb
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-15 23:00:58 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,7 +7,7 @@ end
 def batch_badge_creator(attendees)
 all_badges = []
 for people in 0..attendees
-  all_badges.push(badge_maker(people))
+  all_badges << badge_maker(people)
 end
 all_badges
 end
